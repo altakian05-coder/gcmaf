13431f8a111dddeee4f70d157bec507502a646af
altakian05-coder <altakian05@gmail.com>
2026-08-29 04:01:46 +0600

    Add standard robots.txt with sitemap

---
+++ app/robots.ts
@@ -0,0 +1,26 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: [
+          "Amazonbot",
+          "Applebot-Extended",
+          "Bytespider",
+          "CCBot",
+          "ClaudeBot",
+          "Google-Extended",
+          "GPTBot",
+          "meta-externalagent",
+        ],
+        disallow: "/",
+      },
+    ],
+    sitemap: "https://gcmaf.net/sitemap.xml",
+  };
+}
