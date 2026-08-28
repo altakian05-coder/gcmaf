40f5c49d66359da5afd2536cb010881704b4926c
altakian05-coder <altakian05@gmail.com>
2026-08-29 02:19:06 +0600

    Add g3p and GcMAF article to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -13,6 +13,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
+      url: `${baseUrl}/blog/g3p-and-gcmaf-similarities`,
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
       url: `${baseUrl}/clinical-applications`,
       changeFrequency: "monthly",
       priority: 0.8,
